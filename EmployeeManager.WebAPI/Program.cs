65496c0e34524db3ed37c812817cd77c4498680c
Pavel Kollin <Pavelkollin3@gmail.com>
2026-02-26 13:08:01 +0100

    Fix program.cs

--- EmployeeManager.WebAPI/Program.cs
+++ EmployeeManager.WebAPI/Program.cs
@@ -45,11 +45,11 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 app.UseMiddleware<ApiExceptionMiddleware>();
-// using (var scope = app.Services.CreateScope())
-// {
-//     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-//     await db.Database.MigrateAsync();
-// }
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await db.Database.MigrateAsync();
+}
 
 app.UseCors("Frontend");
 
